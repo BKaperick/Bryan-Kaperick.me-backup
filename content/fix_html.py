16eb190c327ce376dce2e466373edb82a1d79710
bkaperick <bryankaperick@gmail.com>
2023-07-30 14:10:00 +0200

    Fix fix_html, works for generating all html files

--- content/fix_html.py
+++ content/fix_html.py
@@ -1,21 +1,26 @@
 import sys
 import os
 import pathlib
+from itertools import chain
 
 # Usage: python3 fix_html.py path/to/input.html
 
 GLOBAL_REPLACES = {
-        "/images/logo.png": "../../../static/logo.png"
-        '<link rel="stylesheet" href="/css/styles.28722f0782c05e6eb06366f3f97b2b7f1285a6cdbecada724ee0be51706012ae.css">': '<link rel="stylesheet" href="../../../style.css">'
-        '<link rel="stylesheet" href="/css/print.27fc184f8670f41a2690985390779e7b20335a8fadff8fa015cf9417ffe50c36.css" media="print">', ''
+        "/images/logo.png": "../../../static/logo.png",
+        '<link rel="stylesheet" href="/css/styles.28722f0782c05e6eb06366f3f97b2b7f1285a6cdbecada724ee0be51706012ae.css">': '<link rel="stylesheet" href="../../../style.css">',
+        '<link rel="stylesheet" href="/css/print.27fc184f8670f41a2690985390779e7b20335a8fadff8fa015cf9417ffe50c36.css" media="print">': ''
         }
 
 def process_file(fpath):
-    with open(fpath, 'rw') as f:
-        content = '\n'.join(f.readlines())
+    content = ''
+    with open(fpath, 'r') as f:
+        content = ''.join(f.readlines())
         for orig,repl in GLOBAL_REPLACES.items():
             content.replace(orig, repl)
-        f.write(content)
+    with open(fpath, 'w') as f:
+        if content:
+            print("writing content")
+            f.write(content)
 
 if __name__ == '__main__':
 
@@ -23,7 +28,7 @@ if __name__ == '__main__':
         fpath = sys.argv[1]
         process_file(fpath)
     else:
-        for p,s,f in os.walk('./en/') + os.walk('./fr/'):
+        for p,s,f in chain(os.walk('./en/'), os.walk('./fr/')):
             for name in f:
                 if '.html' in name:
                     process_file(pathlib.PurePath(p,name))
