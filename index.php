aca751dd9420250d604c6a14c4c619730c8d756c
bkaperick <bryankaperick@gmail.com>
2024-07-04 13:07:09 +0200

    CSS is working now

--- index.php
+++ index.php
@@ -8,16 +8,17 @@ $lang = $_GET["lang"] ?? "en";
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width">
-<link rel="stylesheet" href="style.css">
+<style>
+  <?php include "style.css" ?>
+</style>
 <?php 
 
 $language = $_GET["lang"] ?? "en";
 require_once("./view/Language/lang.".$lang.".php");
-
-
 readfile("./content/".$lang."/mainpage.html")
 ?>
 <title><?php echo $language["SITE_TITLE"]; ?></title>
+<body> test body </body>
 <div class="demo-page-title"><?php echo $language["POETRY_TITLE"]; ?></div>
 
 </html>
