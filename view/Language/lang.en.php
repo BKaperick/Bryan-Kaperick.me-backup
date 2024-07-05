d42b39a02bff2d38ccfe7cee136b1e4674f3856b
bkaperick <bryankaperick@gmail.com>
2024-07-05 16:15:08 +0200

    Main page language toggling and header/footer refactor is working

--- view/Language/lang.en.php
+++ view/Language/lang.en.php
@@ -1,3 +1,9 @@
 <?php
-$language = array('POETRY_TITLE'=>"Poetry", 'SITE_TITLE'=>"Bryan Kaperick&#39;s Website");
+$language = array(
+    'POETRY_TITLE'=>"Poetry", 
+    'SITE_TITLE'=>"Bryan Kaperick&#39;s Website",
+    'LANGUAGE_TOGGLE'=>'Euh, il n&#39;y a pas une version Français, hein ?',
+    'Datapoint'=>'Daily data point',
+    'Slang'=>'Daily French slang'
+);
 ?>
--- view/Language/lang.fr.php
+++ view/Language/lang.fr.php
@@ -1,4 +1,10 @@
 
 <?php
-$language = array('POETRY_TITLE'=>"La Poésie", 'SITE_TITLE'=>"Le Site Web de Bryan Kaperick");
+$language = array(
+    'POETRY_TITLE'=>"La Poésie", 
+    'SITE_TITLE'=>"Le Site Web de Bryan Kaperick",
+    'LANGUAGE_TOGGLE'=>'Uh, in English please...?',
+    'Datapoint'=>'Le point de donnée quotidien',
+    'Slang'=>'L’Argot quotidien'
+);
 ?>
