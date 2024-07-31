27ecbba83924a6efa6b91a06152538502d15dcf0
Bryan Kaperick <bryankaperick@gmail.com>
2024-07-31 16:44:11 +0200

    First test with auto-format of poems.json

--- content/poems/format_poem.py
+++ content/poems/format_poem.py
@@ -2,8 +2,8 @@ import sys
 import json
 
 # python format_poem.py input.txt output.md
-fname = sys.argv[1]
-fname_out = sys.argv[2]
+#fname = sys.argv[1]
+#fname_out = sys.argv[2]
 
 def format_line_markdown(line):
     line = ">" + line.strip()
@@ -15,19 +15,20 @@ def format_line(line):
     line = line.replace("--", "&mdash;")
     return line
 
-with open("poems.json, 'r+') as fwrite:
+with open("poems.json", 'r+') as fwrite:
     poems = json.load(fwrite)
     for name, poem_metadata in poems.items():
-        if not poem_metadata.haskey("body"):
+        if not "body" in poem_metadata:
+            print("Auto-formatting " + poem_metadata['title'] + " from " + poem_metadata['rawpath'])
             with open(poem_metadata['rawpath'], 'r') as fin:
                 poem = []
                 for line in fin.readlines():
                     poem.append(format_line(line))
                 "<br>\n".join(poem)
-                fwrite[name]['body'] = poem
+                poems[name]['body'] = poem
 
     fwrite.seek(0)
-    json.dump(data, fwrite, indent=4)
+    json.dump(poems, fwrite, indent=4)
     fwrite.truncate()
 
 
