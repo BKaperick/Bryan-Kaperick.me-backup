05d4fb62865450a221d72cb7c0c9ea2ee78019a1
Bryan Kaperick <bryankaperick@gmail.com>
2024-07-31 17:09:31 +0200

    progress

--- content/poems/format_poem.py
+++ content/poems/format_poem.py
@@ -9,11 +9,26 @@ def format_line_markdown(line):
     line = ">" + line.strip()
     return format_line(line) + "  \n"
 
+def format_poem(poem):
+    out = []
+    for line in poem:
+        out.append(format_line(line))
+    for i,l in enumerate(out):
+        if i > 0 and l == "<p>\n<\\p>":
+            out[i-1] = out[i-1].replace("<br>", "")
+    out[-1] = out[-1].replace("<br>", "")
+    poem_body = "<p>" + "\n".join(poem) + r"<\p>"
+    return poem_body
+
+
 def format_line(line):
     line = line.strip()
+    print(line)
+    if not line:
+        return "<p>\n<\\p>"
     line = line.replace("    ", "&nbsp;&nbsp;&nbsp;")
     line = line.replace("--", "&mdash;")
-    return line
+    return line + "<br>\n"
 
 with open("poems.json", 'r+') as fwrite:
     poems = json.load(fwrite)
@@ -21,15 +36,22 @@ with open("poems.json", 'r+') as fwrite:
         if not "body" in poem_metadata:
             print("Auto-formatting " + poem_metadata['title'] + " from " + poem_metadata['rawpath'])
             with open(poem_metadata['rawpath'], 'r') as fin:
-                poem = []
-                for line in fin.readlines():
-                    poem.append(format_line(line))
-                "<br>\n".join(poem)
-                poems[name]['body'] = poem
+                poem_body = format_poem(list(fin.readlines()))
+                poems[name]['body'] = poem_body
 
     fwrite.seek(0)
     json.dump(poems, fwrite, indent=4)
     fwrite.truncate()
 
+fixes = []
+with open ("poems.json", 'r') as f:
+    for line in f.readlines():
+        fixes.append(line.replace("\\\\", "\\"))
+
+with open ("poems.json", 'w') as f:
+    f.writelines(fixes)
+
+    
+        
 
 
