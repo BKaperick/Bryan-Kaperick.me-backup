b737e8be6683bdd991a1646b2e63404cbea91728
Bryan Kaperick <bryankaperick@gmail.com>
2024-07-31 14:43:35 +0200

    Started converting some of my poems

--- content/poetry.php
+++ content/poetry.php
@@ -4,11 +4,8 @@ global $language;
 require_once("../view/Language/lang.".$lang.".php");
 
 $string = file_get_contents("./poems/poems.json");
-$poems = json_decode($string);
-$night = $poems->night;
-$chapman = $poems->chapman;
-$kubla = $poems->kubla;
-$complim = $poems->compliment;
+$p = json_decode($string);
+
 include($_SERVER['DOCUMENT_ROOT']."/content/header.php");
 include($_SERVER['DOCUMENT_ROOT']."/content/".$lang."/poetry/poetry.html");
 include($_SERVER['DOCUMENT_ROOT']."/content/footer.php");
