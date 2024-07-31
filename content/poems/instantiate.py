624459df3c857f51a1d2d55f1d0bd016b54d7287
Bryan Kaperick <bryankaperick@gmail.com>
2024-07-31 18:05:40 +0200

    instantiate works v0.1

--- content/poems/instantiate.py
+++ content/poems/instantiate.py
@@ -2,14 +2,28 @@ import json
 import os
 import datetime
 
-for file in os.listdir("./new/"):
-    d = {"title": ""}
-    with open("poems.json", "r+") as fw:
-        poems = json.load(fwrite)
-        poems['new_' + datetime.utcnow().strftime('%Y%m%d_%H%M%S')] = d
-        fw.seek(0)
-        json.dump(poems, fw, indent=4)
-        fw.truncate()
+
+count = 1
+with open("poems.json", "r+") as fw:
+    poems = json.load(fw)
+
+    for file in os.listdir("./new/"):
+        print(file)
+        name_words = file.replace(".txt", "").split("_")
+        longest_word = max(name_words, key=len)
+        name_guess = " ".join(name_words)
+        name_guess = name_guess[0].capitalize() + name_guess[1:]
+        d = {
+            "title": name_guess,
+            "author": "Bryan Kaperick",
+            "month": str(datetime.datetime.now().strftime("%B"))[:3],
+            "year": str(datetime.datetime.now().year)
+            }
+        poems[longest_word + '_' + str(count)] = d
+        count += 1
         
 
 
+    fw.seek(0)
+    json.dump(poems, fw, indent=4)
+    fw.truncate()
