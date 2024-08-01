8de212919095c2ad390f119238438043eee1b16d
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-01 14:20:14 +0200

    tested, instantiate works.

--- content/photos/instantiate.py
+++ content/photos/instantiate.py
@@ -10,15 +10,15 @@ with open("photos.json", "r+") as fw:
     photos = json.load(fw)
     for file in os.listdir("./new/"):
         print(file)
-        name_words = [clean_key(w) for w in file.replace(".txt", "").split("_")]
+        name_words = [clean_key(w) for w in file.replace(".txt", "").replace(".jpg", "").split("_")]
         longest_word = max(name_words, key=len)
         name_guess = " ".join(name_words)
         name_guess = name_guess[0].capitalize() + name_guess[1:]
         year = datetime.datetime.now().year
         if count == 1:
-            last_photo = max([(k,v) for k,v in photos.items() if v['year'] == year], key=v['order_in_year'])
+            last_photo = max([(k,v) for k,v in photos.items() if v['year'] == year], key=lambda x : x[1]['order_in_year'])
             if last_photo:
-                order_in_year = last_photo['order_in_year'] + 1
+                order_in_year = last_photo[1]['order_in_year'] + 1
                 count = order_in_year 
 
         d = {
