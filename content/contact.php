04baaa3d57b5770ab00832872a311c8db6eb4cb5
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-01 15:16:55 +0200

    fixing up bio in french and english plus changes since copy-paste in content

--- content/contact.php
+++ content/contact.php
@@ -1,5 +1,8 @@
 <?php 
 $lang = $_GET["lang"] ?? "en";
+global $language;
+require_once("../view/Language/lang.".$lang.".php");
+
 include($_SERVER['DOCUMENT_ROOT']."/content/header.php");
 include($_SERVER['DOCUMENT_ROOT']."/content/".$lang."/contact/contact.html");
 include($_SERVER['DOCUMENT_ROOT']."/content/footer.php");
