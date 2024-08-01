009ce362ced35085525b3b1fe0ab1c6b9a7a70a7
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-01 10:01:39 +0200

    Fixing poem spacing formatting

--- content/poems/format_poem.py
+++ content/poems/format_poem.py
@@ -1,6 +1,14 @@
 import sys
 import json
 
+target = []
+format_all = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'all-poems':
+        format_all = True
+    else:
+        target = " ".join(sys.argv[1:])
+
 def format_poem(poem):
     out = []
     for line in poem:
@@ -14,7 +22,7 @@ def format_poem(poem):
 
 
 def format_line(line):
-    line = line.strip()
+    line = line.rstrip()
     if not line:
         return "</p>\n<p>"
     line = line.replace("    ", "&nbsp;&nbsp;&nbsp;")
@@ -24,7 +32,7 @@ def format_line(line):
 with open("poems.json", 'r+') as fwrite:
     poems = json.load(fwrite)
     for name, poem_metadata in poems.items():
-        if not "body" in poem_metadata:
+        if (format_all and 'rawpath' in poem_metadata) or (not "body" in poem_metadata) or (name in target):
             print("Auto-formatting \"" + poem_metadata['title'] + "\" from " + poem_metadata['rawpath'])
             with open(poem_metadata['rawpath'], 'r') as fin:
                 poem_body = format_poem(list(fin.readlines()))
