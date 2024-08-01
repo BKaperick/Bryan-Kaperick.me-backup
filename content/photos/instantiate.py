317c4af097fc5e7a0ed9be967a688aed7f9fa248
Bryan Kaperick <bryankaperick@gmail.com>
2024-08-01 11:27:06 +0200

    Instantiate v0.1 working, english description, year, and path copied

--- content/photos/instantiate.py
+++ content/photos/instantiate.py
@@ -7,12 +7,14 @@ with open('photos_old.html', 'r') as f:
         content = "".join(f.readlines()).split("\n\n")
         count = 0
         for photo_elem in content:
+            photo_elem = photo_elem.replace("\n", "")
             count += 1
             m = re.search(r'<img src="../../../static/(.*)">', photo_elem)
-            oldpath = "../../../static/" + m.group(0)
-            rawpath = "./photos/raw/" + m.group(0)
+            oldpath = "../../../static/" + m.group(1)
+            rawpath = "./photos/raw/" + m.group(1)
             m_en = re.search(r'<figcaption>(.*)</figcaption>', photo_elem)
-            en,year_str = m_en.group(0).split(' ~ ')
+            print(m_en.group(1))
+            en,year_str = m_en.group(1).split(' ~ ')
             
             desc_words = en.split(" ")
             longest_word = max(desc_words, key=len)
@@ -23,10 +25,10 @@ with open('photos_old.html', 'r') as f:
                 "en": en,
                 "people": [],
                 }
-            key = longest_word + str(year) + "_" + count
+            key = longest_word + year_str + "_" + str(count)
             photos[key] = d
-            
-            os.rename(oldpath, "." + rawpath)
+            print("`mv {0} {1}`".format(oldpath, "." + rawpath))
+            #os.rename(oldpath, "." + rawpath)
         
         fwrite.seek(0)
         json.dump(photos, fwrite, indent=4)
